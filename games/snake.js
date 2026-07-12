69649e00293bd3ea83e7fb2dc70a78c753617d15
Mike <mike.knoppix@gmail.com>
2026-07-12 14:50:23 +0200

    Update snake.js

--- games/snake.js
+++ games/snake.js
@@ -1,1 +1,27 @@
+class SnakeGame {
 
+    constructor(canvas) {
+
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+
+    }
+
+    start() {
+
+        this.ctx.fillStyle = "#111";
+        this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+
+        this.ctx.fillStyle = "#42ff42";
+        this.ctx.font = "24px Arial";
+        this.ctx.textAlign = "center";
+
+        this.ctx.fillText(
+            "Snake próximamente...",
+            this.canvas.width/2,
+            this.canvas.height/2
+        );
+
+    }
+
+}
