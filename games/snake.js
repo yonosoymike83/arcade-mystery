dbb35f449f45e01bed73410b9817bb1bd4618e06
Mike <mike.knoppix@gmail.com>
2026-07-12 15:18:43 +0200

    Update snake.js

--- games/snake.js
+++ games/snake.js
@@ -1,26 +1,83 @@
 class SnakeGame {
 
-    constructor(canvas) {
+    constructor(canvas, challenge) {
 
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
 
+        this.size = 20;
+
+        this.snake = [
+            {x:10,y:10},
+            {x:9,y:10},
+            {x:8,y:10}
+        ];
+
+        this.food = {
+            x:15,
+            y:10
+        };
+
     }
 
-    start() {
+    start(){
 
-        this.ctx.fillStyle = "#111";
-        this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+        this.draw();
 
-        this.ctx.fillStyle = "#42ff42";
-        this.ctx.font = "24px Arial";
-        this.ctx.textAlign = "center";
+    }
 
-        this.ctx.fillText(
-            "Snake próximamente...",
-            this.canvas.width/2,
-            this.canvas.height/2
+    draw(){
+
+        this.ctx.fillStyle="#111";
+        this.ctx.fillRect(0,0,400,400);
+
+        // Cuadrícula
+
+        this.ctx.strokeStyle="#1f1f1f";
+
+        for(let i=0;i<=400;i+=this.size){
+
+            this.ctx.beginPath();
+            this.ctx.moveTo(i,0);
+            this.ctx.lineTo(i,400);
+            this.ctx.stroke();
+
+            this.ctx.beginPath();
+            this.ctx.moveTo(0,i);
+            this.ctx.lineTo(400,i);
+            this.ctx.stroke();
+
+        }
+
+        // Comida
+
+        this.ctx.fillStyle="red";
+
+        this.ctx.fillRect(
+
+            this.food.x*this.size,
+            this.food.y*this.size,
+            this.size,
+            this.size
+
         );
+
+        // Serpiente
+
+        this.ctx.fillStyle="#42ff42";
+
+        for(const part of this.snake){
+
+            this.ctx.fillRect(
+
+                part.x*this.size,
+                part.y*this.size,
+                this.size,
+                this.size
+
+            );
+
+        }
 
     }
 
